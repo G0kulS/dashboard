c21e840469c4ce9a1ec6fdbe34c18d3fd7a0e9d8
Gokul s <samigokul474@gmail.com>
2021-03-23 11:31:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+import Lside from "./sidebar";
+import Tbar from "./topbar";
+import Crow from "./contentrow";
+import Card from "./card"
+import Chart from "./chart";
+import User from "./user";
+import Createuser from "./createuser";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return ( 
+    <Router>
+    <div id="wrapper">
+      <Lside></Lside>
+      <div id="content">
+       <Tbar></Tbar>
+       <Switch>
+         <Route path ="/" exact="true">
+       <div className="container-fluid">
+       <Crow></Crow>
+       <Card></Card>
+       <Chart></Chart>
+       </div>
+       </Route>
+       <Route path="/user" exact="true">
+        <User></User>
+        </Route>
+        <Route path="/createuser" exact="true">
+        <Createuser></Createuser>
+        </Route>
+        <Route path="/edituser:id" exact="true">
+        <div>Edit-user</div>
+        </Route>
+       </Switch>
+        </div>  
+        </div>
+        </Router>
+            );
+}
+
+export default App;
